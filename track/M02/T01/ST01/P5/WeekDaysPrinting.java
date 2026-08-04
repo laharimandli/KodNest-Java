6588eb003f9a935174442eac482df336f9f92b28
laharimandli <laharimandli@gmail.com>
2026-08-04 11:32:04 +0530

    WeeklyDaysPrinting.java

---
+++ track/M02/T01/ST01/P5/WeekDaysPrinting.java
@@ -0,0 +1,14 @@
+
+public class WeekDaysPrinting {
+
+    public static void main(String args[]) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursaday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+
+    }
+}
