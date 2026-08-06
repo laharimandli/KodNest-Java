dea18e0c9ad02d84b503a43fc4cb3182da6e257a
laharimandli <laharimandli@gmail.com>
2026-08-06 11:43:32 +0530

    Added NestedDoWhile

--- track/M02/T01/ST02/P10/NestedDoWhile.java
+++ track/M02/T01/ST02/P10/NestedDoWhile.java
@@ -4,6 +4,7 @@ public class NestedDoWhile {
     public static void main(String[] args) {
         int i = 1;
         do {
+
             int j = 1;
             do {
                 System.out.println(j);
@@ -12,5 +13,6 @@ public class NestedDoWhile {
             System.out.println();
             i++;
         } while (i <= 5);
+
     }
 }
