74043b89d9395aaca28871a1ac624ec43b6d9edc
laharimandli <laharimandli@gmail.com>
2026-08-06 11:41:40 +0530

    Added NestedWhile

---
+++ track/M02/T01/ST02/P9/NestedWhile.java
@@ -0,0 +1,18 @@
+
+public class NestedWhile {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            int j = 1;
+            while (j <= 5) {
+                System.out.println(j);
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
+
+    }
+
+}
