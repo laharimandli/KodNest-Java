9ca0d23605427ad0b6a226d15a4c0706e69f9a51
laharimandli <laharimandli@gmail.com>
2026-08-10 09:38:06 +0530

    Personal Calculator

--- track/M02/T01/ST03/P6/PersonalExpenseCalculator.java
+++ track/M02/T01/ST03/P6/PersonalExpenseCalculator.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class PersonalCalculator {
+public class PersonalExpenseCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
