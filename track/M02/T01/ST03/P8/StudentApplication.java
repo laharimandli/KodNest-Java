bbc0ba8f062bbb119aa141a118e8387b145aff21
laharimandli <laharimandli@gmail.com>
2026-08-10 13:19:28 +0530

    Student Application

---
+++ track/M02/T01/ST03/P8/StudentApplication.java
@@ -0,0 +1,30 @@
+
+class StudentApplication {
+
+    public static void main(String args[]) {
+        Student s1 = new Student();
+        s1.roll = 12;
+        s1.name = "Lahari";
+        s1.height = 5.5;
+        System.out.println(s1.roll);
+        System.out.println(s1.name);
+        System.out.println(s1.height);
+        s1.run();
+        s1.sleep();
+    }
+}
+
+class Student {
+
+    int roll;
+    String name;
+    double height;
+
+    void run() {
+        System.out.println("Running");
+    }
+
+    void sleep() {
+        System.out.println("Sleeping");
+    }
+}
