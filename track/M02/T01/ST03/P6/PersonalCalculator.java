0ff8a89eb78e30dddd4ac63dc0f8c8cb5170c99a
laharimandli <laharimandli@gmail.com>
2026-08-10 09:37:03 +0530

    Personal Calculator

---
+++ track/M02/T01/ST03/P6/PersonalCalculator.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class PersonalCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double monthlyIncome = scanner.nextDouble();
+        double rentExpense = scanner.nextDouble();
+        double foodExpense = scanner.nextDouble();
+        double travelExpense = scanner.nextDouble();
+
+        double total = foodExpense + rentExpense + travelExpense;
+        double rem = monthlyIncome - total;
+
+        System.out.println("Total expense: " + total);
+        System.out.println("Remaining: " + rem);
+
+        String status = "";
+
+        if (rem >= 0) {
+            status = "Within budget";
+        } else {
+            status = "Over budget";
+        }
+
+        System.out.println("Status: " + status);
+
+        scanner.close();
+    }
+}
