81b8b1461281f2152e1b482f561171555b1c1b65
laharimandli <laharimandli@gmail.com>
2026-08-11 11:16:43 +0530

    Employee Application

---
+++ track/M02/T01/ST04/P2/EmployeeApplication.java
@@ -0,0 +1,31 @@
+
+class EmployeeApplication {
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.id = 11;
+        e1.name = "Lahari";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+
+        Employee e2 = e1;
+        e2.id = 22;
+        e2.name = "Chintu";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        System.out.println(e2.id);
+        System.out.println(e2.name);
+
+    }
+}
+
+class Employee {
+
+    int id;
+    String name;
+
+    void work() {
+        System.out.println("Working");
+    }
+
+}
