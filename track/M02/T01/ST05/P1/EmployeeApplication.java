6286638bb200ef73ab80b92521127d1e4d5e7e5e
laharimandli <laharimandli@gmail.com>
2026-08-12 12:25:43 +0530

    Employee Application

---
+++ track/M02/T01/ST05/P1/EmployeeApplication.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+class EmployeeApplication {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        double salary = sc.nextDouble();
+
+        Employee e1 = new Employee();
+        e1.setDetails(name, salary);
+        e1.displayDetails();
+    }
+}
+
+class Employee {
+
+    String name;
+    double salary;
+
+    void setDetails(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Salary: " + salary);
+    }
+}
