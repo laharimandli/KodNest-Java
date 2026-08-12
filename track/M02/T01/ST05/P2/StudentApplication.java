4a835996d366ba1978c062ab67fa851f362604dc
laharimandli <laharimandli@gmail.com>
2026-08-12 12:35:49 +0530

    Store values in Student Class

---
+++ track/M02/T01/ST05/P2/StudentApplication.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+class StudentApplication {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+
+        Student s1 = new Student();
+        s1.setName(name);
+        s1.displayName();
+    }
+}
+
+class Student {
+
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void displayName() {
+        System.out.println("Student Name: " + name);
+    }
+}
