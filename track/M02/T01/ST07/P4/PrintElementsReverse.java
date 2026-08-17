d8771972d43001efcdb70df28efccaa3e1e8bfe3
laharimandli <laharimandli@gmail.com>
2026-08-17 13:15:26 +0530

    Print Array Elements In Reverse Order

---
+++ track/M02/T01/ST07/P4/PrintElementsReverse.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class PrintElementsReverse {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("Enter Array Elements: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements are: ");
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.println(a[i]);
+        }
+    }
+}
