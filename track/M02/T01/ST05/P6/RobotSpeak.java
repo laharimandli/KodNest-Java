9b227e0812d38963533e9fd50170e2c278088a97
laharimandli <laharimandli@gmail.com>
2026-08-17 09:10:34 +0530

    RobotSpeak

---
+++ track/M02/T01/ST05/P6/RobotSpeak.java
@@ -0,0 +1,19 @@
+
+class RobotSpeak {
+
+    public static void main(String[] args) {
+        // Create a Robot object
+        Robot robot = new Robot();
+
+        // Call speak()
+        robot.speak();
+    }
+}
+
+class Robot {
+
+    void speak() {
+        // Print the message
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+}
