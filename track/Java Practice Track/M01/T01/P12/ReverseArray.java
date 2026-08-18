4385e6f7bae78ab336f827cc7971aade53145163
laharimandli <laharimandli@gmail.com>
2026-08-18 08:15:44 +0530

    Reverse Array

---
+++ track/Java Practice Track/M01/T01/P12/ReverseArray.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int[] scores = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            scores[i] = sc.nextInt();
+        }
+
+        System.out.print("Reverse scores: ");
+
+        for (int i = size - 1; i >= 0; i--) {
+            System.out.print(scores[i] + " ");
+        }
+    }
+}
