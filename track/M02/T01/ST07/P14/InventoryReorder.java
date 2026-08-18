e08b062fd0502366a963a4681852ee5c1187f4b7
laharimandli <laharimandli@gmail.com>
2026-08-18 13:08:57 +0530

    Inventory Reorder

---
+++ track/M02/T01/ST07/P14/InventoryReorder.java
@@ -0,0 +1,17 @@
+
+public class InventoryReorder {
+
+    public static void main(String[] args) {
+        int[] stock = {4, 12, 2, 9, 5};
+        int reorderLevel = 5;
+        int reorderCount = 0;
+
+        for (int i = 0; i < stock.length; i++) {
+            if (stock[i] <= reorderLevel) {
+                reorderCount++;
+            }
+        }
+
+        System.out.println("Items to reorder: " + reorderCount);
+    }
+}
