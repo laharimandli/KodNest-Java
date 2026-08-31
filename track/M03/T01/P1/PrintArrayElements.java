66bf23c9d100484b49f10187773b6d1e1a45536a
laharimandli <laharimandli@gmail.com>
2026-08-31 18:58:21 +0530

    Print Array Elements

---
+++ track/M03/T01/P1/PrintArrayElements.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class PrintArrayElements {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (i > 0) {
+                System.out.print(" ");
+            }
+            System.out.print(arr[i]);
+        }
+
+        sc.close();
+    }
+}
