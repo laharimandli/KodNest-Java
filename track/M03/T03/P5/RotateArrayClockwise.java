062c8ba96c3ed74c4a1603ca96db935e836d0eb0
laharimandli <laharimandli@gmail.com>
2026-09-01 15:20:07 +0530

    Rotate Array Anti clockwise once

--- track/M03/T03/P5/RotateArrayClockwise.java
+++ track/M03/T03/P5/RotateArrayClockwise.java
@@ -1,18 +1,18 @@
 
 import java.util.Scanner;
 
-class RotateArrayAnticlockwise {
+class RotateArrayClockwise {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int a[] = {12, 23, 34, 45};
-        int temp = a[0];
-        for (int i = 1; i < a.length; i++) {
-            a[i - 1] = a[i];
+        int temp = a[a.length - 1];
+        for (int i = 2; i >= 0; i--) {
+            a[i + 1] = a[i];
         }
-        a[a.length - 1] = temp;
+        a[0] = temp;
 
-        System.out.println(" Array Elements are: ");
+        System.out.println("Array Elements are: ");
         for (int num : a) {
             System.out.print(num + " ");
         }
