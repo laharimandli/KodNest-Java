7516c4462410a697e5703cf3be7445313b764b0e
laharimandli <laharimandli@gmail.com>
2026-09-03 14:30:10 +0530

    Rotate Array Left By K

--- track/M03/T03/P10/RotateArrayLeftByK.java
+++ track/M03/T03/P10/RotateArrayLeftByK.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 public class RotateArrayLeftByK {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         int size = sc.nextInt();
-
         int arr[] = new int[size];
 
         for (int i = 0; i < size; i++) {
@@ -17,48 +15,16 @@ public class RotateArrayLeftByK {
 
         int k = sc.nextInt();
 
-        k = k % size;
+        for (int i = 1; i <= k; i++) {
+            int temp = arr[0];
 
-        // Reverse first k elements
-        int i = 0;
-        int j = k - 1;
+            for (int j = 1; j < size; j++) {
+                arr[j - 1] = arr[j];
+            }
 
-        while (i < j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
-            i++;
-            j--;
+            arr[size - 1] = temp;
         }
 
-        // Reverse remaining elements
-        i = k;
-        j = size - 1;
-
-        while (i < j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
-            i++;
-            j--;
-        }
-
-        // Reverse entire array
-        i = 0;
-        j = size - 1;
-
-        while (i < j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
-            i++;
-            j--;
-        }
-
-        // Print array
         for (int num : arr) {
             System.out.print(num + " ");
         }
---
+++ track/M03/T03/P11/Palindrome.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean isPalindrome = true;
+
+        int i = 0;
+        int j = size - 1;
+
+        while (i < j) {
+
+            if (arr[i] != arr[j]) {
+                isPalindrome = false;
+                break;
+            }
+
+            i++;
+            j--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
