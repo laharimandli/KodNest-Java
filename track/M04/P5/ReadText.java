82a90c910af021710821f0452d5d0ca2e3de313d
laharimandli <laharimandli@gmail.com>
2026-09-07 17:43:04 +0530

    Read Text using Scanner

---
+++ track/M04/P5/ReadText.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class ReadText {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String learnerName = scanner.nextLine();
+        String city = scanner.nextLine();
+
+        System.out.println("Name: " + learnerName);
+        System.out.println("City: " + city);
+        System.out.println("Name length: " + learnerName.length());
+    }
+}
