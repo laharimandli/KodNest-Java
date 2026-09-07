bd66aa24cad0233e49c30c3172c7ed43c8a029c4
laharimandli <laharimandli@gmail.com>
2026-09-07 17:27:58 +0530

    String 3

---
+++ track/M04/P3/String3.java
@@ -0,0 +1,13 @@
+
+public class String3 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        if (s1.equals(s2)) {
+            System.out.println("References are equal");
+        } else {
+            System.out.println("References are not equal");
+        }
+    }
+}
