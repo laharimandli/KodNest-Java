f560664cde3bf608763d9274d237b23110541bcd
laharimandli <laharimandli@gmail.com>
2026-09-08 16:56:43 +0530

    Compare Strings

---
+++ track/M04/P7/CompareStrings.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstText = scanner.nextLine();
+        String secondText = scanner.nextLine();
+
+        System.out.println("Exact match: " + firstText.equals(secondText));
+        System.out.println("Ignore-case match: " + firstText.equalsIgnoreCase(secondText));
+    }
+}
