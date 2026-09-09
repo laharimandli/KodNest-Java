12ec5b09649b7507698df68af8883a50bca83309
laharimandli <laharimandli@gmail.com>
2026-09-09 16:50:54 +0530

    String Methods 3

---
+++ track/M04/P8/StringMethod3.java
@@ -0,0 +1,11 @@
+
+class StringMethod3 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String res = s1.toUpperCase();
+        System.out.println(res);
+        System.err.println(s1);
+    }
+
+}
