3511efd239c5a4606ce8460f6bcbf0e73e0999ca
laharimandli <laharimandli@gmail.com>
2026-09-10 17:08:34 +0530

    String Builder3

---
+++ track/M04/P9/StringBuilder3.java
@@ -0,0 +1,20 @@
+
+class StringBuilder3 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+        sb.ensureCapacity(100);
+        System.out.println(sb.capacity());
+        System.out.println(sb);
+        sb.append("Program");
+        System.out.println(sb);
+        sb.insert(0, "Python");
+        System.out.println(sb);
+        sb.delete(0, 4);
+        System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
+
+    }
+
+}
