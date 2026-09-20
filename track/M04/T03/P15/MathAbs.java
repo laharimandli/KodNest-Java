8edf7ef3dca18db568e6619dfc3960a4395c4263
laharimandli <laharimandli@gmail.com>
2026-09-20 13:42:13 +0530

    Math Abs

---
+++ track/M04/T03/P15/MathAbs.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class MathAbs {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        System.out.println("Absolute: " + Math.abs(n1));
+    }
+}
