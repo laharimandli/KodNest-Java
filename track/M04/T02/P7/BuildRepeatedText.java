a35e9f5ba84431b555bf26f2b9e19f711f878235
laharimandli <laharimandli@gmail.com>
2026-09-20 13:06:33 +0530

    Build Repeated Text

---
+++ track/M04/T02/P7/BuildRepeatedText.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class BuildRepeatedText {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine();
+        int repeatCount = Integer.parseInt(scanner.nextLine());
+
+        StringBuilder builder = new StringBuilder();
+
+        // Append word repeatCount times.
+        for (int i = 0; i < repeatCount; i++) {
+            builder.append(word);
+        }
+
+        System.out.println("Result: " + builder);
+    }
+}
