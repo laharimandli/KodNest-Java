de013f322bd8967192d105da87337b472b1aff30
laharimandli <laharimandli@gmail.com>
2026-09-20 12:09:06 +0530

    Find Missing Number

---
+++ track/M03/T04/P1/MissingNumber.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int orgsum = n * (n + 1) / 2;
+        int actsum = 0;
+
+        for (int i = 0; i < n; i++) {
+            actsum += arr[i];
+        }
+
+        System.out.println(orgsum - actsum);
+    }
+}
