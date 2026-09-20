e930dec78c5d772d671f542cef6e6ba317539c36
laharimandli <laharimandli@gmail.com>
2026-09-20 13:41:06 +0530

    Find Max Math

---
+++ track/M04/T03/P14/FindMaxMath.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class FindMaxMath {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        System.out.println("Maximum: " + Math.max(n1, n2));
+    }
+}
