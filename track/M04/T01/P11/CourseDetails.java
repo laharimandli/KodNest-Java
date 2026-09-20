da547a165c6706351e540d9d57daa7d96d658c1b
laharimandli <laharimandli@gmail.com>
2026-09-20 12:33:37 +0530

    Course Details

---
+++ track/M04/T01/P11/CourseDetails.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class CourseDetails {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String coursename = sc.nextLine();
+        String trainername = sc.nextLine();
+        System.out.println("Course: " + coursename);
+        System.out.println("Trainer: " + trainername);
+
+        // Read and display the course and trainer names.
+    }
+}
